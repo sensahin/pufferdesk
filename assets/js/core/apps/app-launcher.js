54b39092c8e047d54700cecab81d8c91af6ddf11
Senol Sahin <senols@gmail.com>
2026-06-05 02:25:01 +0300

    Fix OS Settings pane sizing

--- assets/js/core/apps/app-launcher.js
+++ assets/js/core/apps/app-launcher.js
@@ -29,7 +29,7 @@
 			if (app.kind === 'native' && app.native === 'settings') {
 				options.content = window.AdminOSMode.apps.createSettingsApp({ config });
 				options.bodyClass = 'aos-window-body aos-settings-body';
-				options.width = '980px';
+				options.width = '880px';
 				options.height = '680px';
 				return options;
 			}
